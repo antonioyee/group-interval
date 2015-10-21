f1ea5c4d74dbd44e7a92fb191ce20e83da2daa61
ANTONIO YEE <yee.antonio@gmail.com>
2015-10-21 17:37:28 -0600

    validar entradas

--- src/BUS/GroupBundle/Controller/DefaultController.php
+++ src/BUS/GroupBundle/Controller/DefaultController.php
@@ -3,11 +3,45 @@
 namespace BUS\GroupBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller{
 
     public function indexAction(){
         return $this->render('BUSGroupBundle:Default:index.html.twig');
+    }
+
+    public function ordenarAction(){
+        $rango      = $_POST['rango'];
+        $original   = $_POST['group'];
+
+        if ( $original != 0 && $rango != '' ) { // valores existentes
+            $num_elementos = count($original);
+            $num = 0;
+
+            foreach($original as $digito) { // verificar que el arreglo sea valido
+                if( is_numeric($digito) ) {
+                    $num++;
+                }
+            }
+
+            if ( $num == $num_elementos && is_numeric($rango) ) {
+
+                $resultado = array('group_ordenado' => $original, 'ordenado' => TRUE);
+            }else{
+                $resultado = array('mensaje' => 'throw InvalidArgumentException', 'ordenado' => FALSE);
+            }
+        }else{
+            $resultado = array('mensaje' => 'Empty Set', 'ordenado' => FALSE);
+        }
+
+        $response = new Response();
+        $response->setContent(json_encode($resultado));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 
 }
--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,12 +1,45 @@
 $(function(){
+    var group = [];
 
-    $('table#tbl-users').on('click', 'a.btn-detalle', function(){
-        console.log(this);
-		var id = $(this).attr('data-id');
-		$.post('/user/view/' + id , function(data){
-            $('#modal-user').modal('show');
+    $('#btn-add-num').click(function(){
+        group.push($('#numero').val());
+        var cadena = '';
+        group.forEach(function(campo, index) {
+            if ( index == 0 ) {
+                cadena += campo;
+            }else{
+                cadena += ', ' +campo;
+            }
+        });
+        $('#contenedor-set').html('Set: [ ' + cadena + ' ]');
+        $('#numero').val('').focus();
+	});
+
+    $('#btn-ordenar').click(function () {
+        var arreglo = 0;
+        if ( group.length > 0 ) {
+            arreglo = group;
+        }
+
+		$.post('/group-by-interval/ordenar', { rango : $('#rango').val(), group : arreglo }, function(ordenado){
+            var cadena_ordenada = '';
+
+            if ( ordenado.ordenado == true ) {
+                var arreglo         = ordenado.group_ordenado;
+                arreglo.forEach(function(campo, index) {
+                    if ( index == 0 ) {
+                        cadena_ordenada += campo;
+                    }else{
+                        cadena_ordenada += ', ' +campo;
+                    }
+                });
+            }else{
+                cadena_ordenada = ordenado.mensaje;
+            }
+
+            $('#contenedor-set-resultado').html('Set: [ ' + cadena_ordenada + ' ]');
 		},'json');
 		return false;
-	});
+    });
 
 });
