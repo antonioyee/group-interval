939c9fdd926747d5fafa0a4419331bb3da850162
ANTONIO YEE <yee.antonio@gmail.com>
2015-10-22 00:40:56 -0600

    agrupar items

--- src/BUS/GroupBundle/Controller/DefaultController.php
+++ src/BUS/GroupBundle/Controller/DefaultController.php
@@ -31,6 +31,8 @@ class DefaultController extends Controller{
 
                 $ordenado = $this->ordenarGrupo($original, $num_elementos);
 
+                $this->agruparOrdenamiento($rango, $ordenado, $num_elementos);
+
                 $resultado = array('group_ordenado' => $ordenado, 'ordenado' => TRUE);
             }else{
                 $resultado = array('mensaje' => 'throw InvalidArgumentException', 'ordenado' => FALSE);
@@ -50,21 +52,86 @@ class DefaultController extends Controller{
         $menor      = NULL;
         $comodin    = NULL;
 
-        for ($x=0; $x < $num_elementos; $x++) {
-            $menor = $x;
+        for ($x=0; $x < $num_elementos; $x++) { // buscar el menor
+            $menor = $x; // 1er posicion
 
-            for ($y=$x+1; $y <= $num_elementos-1; $y++) {
+            for ($y=$x+1; $y <= $num_elementos-1; $y++) { // comparar con el resto
                 if ( $ordenado[$y] < $ordenado[$menor] ) {
                     $menor = $y;
                 }
             }
 
-            $comodin            = $ordenado[$x];
-            $ordenado[$x]       = $ordenado[$menor];
-            $ordenado[$menor]   = $comodin;
+            /**
+             * Cambiarlos de posicion y volver a hacer el recorrido
+             */
+            $comodin            = $ordenado[$x];        // var temporal como comodin
+            $ordenado[$x]       = $ordenado[$menor];    // menor al inicio
+            $ordenado[$menor]   = $comodin;             // 1er posicion al lugar que tenia el menor
         }
 
         return $ordenado;
+    }
+
+    private function agruparOrdenamiento($rango, $ordenado = array(), $num_elementos){
+        $nvo = array();
+        $rangos = array();
+
+        $min = $ordenado[0];
+        $max = $ordenado[$num_elementos-1];
+
+
+        for ($indice=$min; $indice <= $max; $indice++) {
+            if ( ($indice % $rango) == 0 ) {
+                array_push($rangos, $indice);
+            }
+        }
+
+        print_r($rangos);
+
+        for ($x=0; $x <= count($rangos)-1; $x++) {
+
+            /*if ( $x == 0 ) {
+                echo '[' . $rangos[$x] . ']';
+            }else{
+                if ( $x == count($rangos)-1) {
+                    echo '[' . $rangos[$x] . ']';
+                }else{
+                    echo '[' . $rangos[$x] . ' ' . $rangos[$x+1] . ']';
+                }
+            }*/
+
+            $nvo1 = array();
+            for ($y=0; $y < $num_elementos; $y++) {
+
+                if ( $x == 0 ) {
+
+                    if ( $ordenado[$y] <=  $rangos[$x] ) {
+                        array_push($nvo1, $ordenado[$y]);
+                    }
+
+                }else{
+                    if ( $x == count($rangos)-1) {
+
+                        if ( $ordenado[$y] >  $rangos[$x] ) {
+                            array_push($nvo1, $ordenado[$y]);
+                        }
+
+                    }else{
+
+                        if ( $ordenado[$y] >  $rangos[$x] && $ordenado[$y] <= $rangos[$x+1] ) {
+                            array_push($nvo1, $ordenado[$y]);
+                        }
+
+                    }
+                }
+            }
+
+            if ( count($nvo1) > 0 ) {
+                array_push($nvo, $nvo1);
+            }
+
+        }
+        print_r($nvo);
     }
 
 }
